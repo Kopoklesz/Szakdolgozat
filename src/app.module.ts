1bd6fc5fe5c41b350bd8c3df9da76f7509b9540a
Kuti Bence (uig93116) <bence.2.kuti@continental-corporation.com>
2025-09-17 14:21:30 +0200

    User API Test

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,13 +2,13 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';  // ✅ ÚJ IMPORT
 import { WebshopModule } from './webshop/webshop.module';
 import { UserModule } from './user/user.module';
 import { ProductModule } from './product/product.module';
 import { CartModule } from './cart/cart.module';
 import { PurchaseModule } from './purchase/purchase.module';
 import { DatabaseModule } from './database.module';
-import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
@@ -16,7 +16,7 @@ import { AuthModule } from './auth/auth.module';
       isGlobal: true,
     }),
     DatabaseModule,
-    AuthModule,
+    AuthModule,  // ✅ ÚJ MODUL
     WebshopModule,
     UserModule,
     ProductModule,
