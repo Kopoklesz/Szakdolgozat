c7024b60ca5fe5753b39d4ddc5f69bc411ee0ba4
Kopoklesz <kuti.bence2000@gmail.com>
2025-10-19 21:12:24 +0200

    db

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -4,11 +4,12 @@ import { UserController } from './user.controller';
 import { UserService } from './user.service';
 import { User } from '../entity/user.entity';
 import { UserBalance } from '../entity/user-balance.entity';
+import { Webshop } from '../entity/webshop.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User, UserBalance])],
+  imports: [TypeOrmModule.forFeature([User, UserBalance, Webshop])],
   controllers: [UserController],
   providers: [UserService],
   exports: [UserService],
 })
-export class UserModule {}
+export class UserModule { }
